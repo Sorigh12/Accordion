cf20d1d7e998d0a9b2d9f3b40a2bbfde386a6827
Sorigh <84138511+Sorigh12@users.noreply.github.com>
2024-02-21 13:03:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,70 +1,28 @@
 const data = [
-  {
-    id: "1",
-    question: "What are accordion components?",
-    answer:
-      "Accordion components are user interface elements used for organizing and presenting content in a collapsible manner. They typically consist of a header, content, and an expand/collapse action.",
-  },
-  {
-    id: "2",
-    question: "What are they used for?",
-    answer:
-      "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.",
-  },
-  {
-    id: "3",
-    question: "Accordion as a musical instrument",
-    answer:
-      "The accordion is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands or compresses the bellows, used in various music genres.",
-  },
-  {
-    id: "4",
-    question: "Can I create an accordion component with a different framework?",
-    answer:
-      "Yes of course, it is very possible to create an accordion component with another framework.",
-  },
-];
+    {
+      id  : '1',
+      question: "What was the most streamed show on Netflix in 2020?",
+      answer:
+        "The Umbrella Academy season 2, with 43million streams",
+    },
+    {
+      id  : '2',
+      question: "Typically, what's the strongest muscle in the human body?",
+      answer:
+        "The masseter - the main jaw muscle",
+    },
+    {
+      id  : '3',
+      question: "How many bones does a shark have?",
+      answer:
+        "None (a shark’s skeleton is made entirely of cartilage)",
+    },
+    {
+      id  : '4',
+      question: "How many Olympic gold medals does Sir Mo Farah have?",
+      answer:
+        "Four Olympic gold medals. He first was at the London 2012 Olympic games when he finished the 10,000m in a time of 27:30.42. He won gold the same year for the Men's 5,000m, which he completed in 13:41:66. At the Rio 2016 Olympics, he won another gold medal for the 10,000m, and a fourth gold medal for the 5,000m.",
+    },
+  ];
 
-const accordionWrapper = document.querySelector(".accordion");
-
-function createAccordionData() {
-  accordionWrapper.innerHTML = data
-    .map(
-      (dataItem) => `
-    <div class="accordion_item">
-    <div class="accordion_title">
-    <h3>${dataItem.question}</h3>
-    <i class="fa-solid fa-arrow-down"></i>
-    </div>
-    <div class="accordion_content">
-    <p>${dataItem.answer}</p>
-    </div>
-    </div>
-    `
-    )
-    .join(" ");
-}
-
-createAccordionData();
-
-const getAccordionTitles = document.querySelectorAll(".accordion_title");
-
-console.log("====================================");
-console.log(getAccordionTitles);
-console.log("====================================");
-
-getAccordionTitles.forEach((currentItem) => {
-  currentItem.addEventListener("click", (event) => {
-    if (currentItem.classList.contains("active")) {
-      currentItem.classList.remove("active");
-    } else {
-      let getAlreadyAddedActiveClasses = document.querySelectorAll(".active");
-
-      getAlreadyAddedActiveClasses.forEach((currentActiveItem) => {
-        currentActiveItem.classList.remove("active");
-      });
-
-      currentItem.classList.add("active");
-    }
-  });
-});
+  export default data;
